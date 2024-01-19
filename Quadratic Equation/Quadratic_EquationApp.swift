68d9194e1b1d4bda86a4ac469618bee2f9436873
Zachary Ives <zives@hawk.iit.edu>
2024-01-19 17:04:41 -0600

    Initial Commit

---
+++ Quadratic Equation/Quadratic_EquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quadratic_EquationApp.swift
+//  Quadratic Equation
+//
+//  Created by Phys440Zachary on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Quadratic_EquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
